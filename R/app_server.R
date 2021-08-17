69bfdb31ba9ac218600635aa3c5c748946b26561
csatio <csatio@yahoo.com.br>
2021-08-17 10:33:01 -0300

    fdsgf

--- R/app_server.R
+++ R/app_server.R
@@ -8,8 +8,15 @@ app_server <- function( input, output, session ) {
   # Your application server logic
 
   library(ggplot2)
+  library(rlang)
+
+  #output$hist <- renderPlot({
+  #  hist(diamonds[,input$variavel])
 
   output$hist <- renderPlot({
-    hist(diamonds[,input$variavel])
+    x = parse_quo(input$variavel, env = caller_env())
+    ggplot(diamonds, aes(!!x))+ geom_bar(fill = "#0073C2FF")
+
+
   })
 }
