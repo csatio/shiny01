6b1f5e44cdaf821696f76b237d9270833293bc53
csatio <csatio@yahoo.com.br>
2021-08-17 00:05:12 -0300

    1

--- app.R
+++ app.R
@@ -4,4 +4,4 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-asngolem::run_app() # add parameters here (if any)
+shiny01::run_app() # add parameters here (if any)
